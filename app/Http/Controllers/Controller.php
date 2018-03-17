7bfece502279da6c7e3205a7887a436aa20e145e
Abhishek Prakash <prakashabhishek6262@gmail.com>
2018-03-17 23:29:06 +0530

    Added Controller into Package.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,10 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * @package Resume Builder
+ * @author  Abhishek Prakash <prakashabhishek6262@gmail.com>
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;

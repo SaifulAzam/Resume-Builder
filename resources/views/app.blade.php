cc9be70cf68b34cb7dbd88efdc5a46d1f98770a0
Abhishek Prakash <prakashabhishek6262@gmail.com>
2018-04-17 16:18:08 +0530

    Formatted the code.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 @include('partials._header')
-  <div id="page" class="site-main">
-    @yield('app_content')
-  </div>
+    <div id="page" class="site-main">
+        @yield('app_content')
+    </div>
 @include('partials._footer')

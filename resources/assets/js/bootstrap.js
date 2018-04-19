f291bbb0d5d6bd21cf0bd3d75e62943245bd9c75
Abhishek Prakash <prakashabhishek6262@gmail.com>
2018-04-19 13:37:46 +0530

    Registered CSRF Token with window.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -40,6 +40,9 @@ let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
 	window.axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
+
+	// Set the CSRF-Token to use it in the vue application.
+	window.CSRF_TOKEN = token.content;
 } else {
 	console.error(
 		"CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token"

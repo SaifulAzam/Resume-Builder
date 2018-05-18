a6af80c7b230d3be650b45e4b14bc955f02502ac
Abhishek Prakash <prakashabhishek6262@gmail.com>
2018-05-18 19:22:37 +0530

    Fixed indentation.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Fixes "Specified key was too long" error.
-	    Schema::defaultStringLength(191);
+        Schema::defaultStringLength(191);
     }
 
     /**
